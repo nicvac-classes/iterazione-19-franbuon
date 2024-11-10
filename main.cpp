56445199fd4b133e0e0e24bf2784c40dccca90ed
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-11-10 18:04:19 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int N, divisori, numdivisori;
+    float resto;
+    cout<<"Scrivi il numero"<<endl;
+    cin>>N;
+    divisori=1;
+    numdivisori=0;
+    while(divisori<=N)
+    {
+        resto=N%divisori;
+        if(resto=0)
+        {
+            cout<<"Il divisore è "<<divisori<<endl;
+            numdivisori=numdivisori+1;
+        }
+        divisori=divisori+1;
+    }
+    cout<<"Il numero di divisori è "<<numdivisori<<endl;
+    if(numdivisori=2)
+    {
+        cout<<"Il numero è primo";
+    }
+    else
+    {
+        cout<<"Il numero non è primo";
+    }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
